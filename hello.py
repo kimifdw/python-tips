8e4f4cdd11662ce21e911a32d5224ffbdbf0a986
fudongwei <fudongwei@sanjiang365.com>
2018-01-02 23:16:26 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+# print("hello {0}".format('world'))
+
+# print("The quick brown fox {0}".format("jumps over {0}".format('the lazy dog')))
+
+# print("300 + 400 = {0}".format(300 + 400))
+
+NAME = input()
+print('hello,', NAME)
